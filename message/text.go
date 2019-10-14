66c95067c7af74ee93434367bfd9e99b0f23354f
Chuanjian Wang <me@ckeyer.com>
2019-10-14 18:54:53 +0800

    初始化文本消息消息回复

--- message/text.go
+++ message/text.go
@@ -12,3 +12,11 @@ func NewText(content string) *Text {
 	text.Content = content
 	return text
 }
+
+// NewTextReply 初始化文本消息消息回复
+func NewTextReply(content string) *Reply {
+	return &Reply{
+		MsgType: MsgTypeText,
+		MsgData: NewText(content),
+	}
+}
