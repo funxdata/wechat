df62e08473f38a4aceaf7b61fec5322b069cd7be
zhangjunhu <401967974@qq.com>
2020-03-12 17:37:44 +0800

    解决微信支付使用代金券回调信息验签失败

--- pay/notify_result.go
+++ pay/notify_result.go
@@ -33,6 +33,15 @@ type NotifyResult struct {
 	OutTradeNo    string `xml:"out_trade_no"`
 	Attach        string `xml:"attach"`
 	TimeEnd       string `xml:"time_end"`
+
+	// 下面字段都是可选返回的(详细见微信支付文档), 为空值表示没有返回, 程序逻辑里需要判断
+	DeviceInfo         string `xml:"device_info"`          // 微信支付分配的终端设备号
+	SettlementTotalFee int    `xml:"settlement_total_fee"` // 应结订单金额=订单金额-非充值代金券金额，应结订单金额<=订单金额。
+	CouponFee          int    `xml:"coupon_fee"`           // 代金券金额
+	CouponCount        int    `xml:"coupon_count"`         // 代金券使用数量
+	CouponType0        string `xml:"coupon_type_0"`
+	CouponID0          string `xml:"coupon_id_0"`  //代金券ID
+	CouponFee0         int    `xml:"coupon_fee_0"` //单个代金券支付金额
 }
 
 // NotifyResp 消息通知返回
@@ -63,6 +72,14 @@ func (pcf *Pay) VerifySign(notifyRes NotifyResult) bool {
 	resMap["out_trade_no"] = notifyRes.OutTradeNo
 	resMap["attach"] = notifyRes.Attach
 	resMap["time_end"] = notifyRes.TimeEnd
+	// 非必传项
+	resMap["device_info"] = notifyRes.DeviceInfo
+	resMap["settlement_total_fee"] = notifyRes.SettlementTotalFee
+	resMap["coupon_fee"] = notifyRes.CouponFee
+	resMap["coupon_count"] = notifyRes.CouponCount
+	resMap["coupon_type_0"] = notifyRes.CouponType0
+	resMap["coupon_id_0"] = notifyRes.CouponID0
+	resMap["coupon_fee_0"] = notifyRes.CouponFee0
 	// 支付key
 	sortedKeys := make([]string, 0, len(resMap))
 	for k := range resMap {
